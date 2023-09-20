42519006dd5c587e34d791535db70b27cdbfae71
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-09-21 04:47:45 +0530

    like_spec write update_likes and its pass

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe 'Like Model' do
-    let(:user) { User.create(name: 'Ansar', photo: 'Image', bio: 'Developer', post_counter: 5) }
-    let(:post) { Post.create(title: 'test title', text: 'test text', comment_counter: 1, like_counter: 1) }
+    let(:user) { create(:user, name: 'Ansar', photo: 'Image', bio: 'Developer', post_counter: 0) }
+    let(:post) { create(:post, title: 'test title', text: 'test text', comment_counter: 0, like_counter: 0) }
 
     it 'should have an integer post_id' do
       like = Like.new(user_id: user.id, post_id: post.id)
@@ -15,6 +15,17 @@ RSpec.describe Like, type: :model do
       like = Like.new(user_id: user.id, post_id: post.id)
       like.user_id = 'string'
       expect(like).to_not be_valid
+    end
+
+    describe '#update_likes' do
+      it 'should increment the like_counter of the associated post' do
+        like = create(:like, user:, post:)
+
+        expect do
+          like.send(:update_likes)
+          post.reload
+        end.to change(post, :like_counter).by(1)
+      end
     end
   end
 end
