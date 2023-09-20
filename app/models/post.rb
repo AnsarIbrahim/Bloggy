f7cceaab72d867cfd43a0ba05cdb6af5cab09193
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-09-20 23:02:19 +0530

    Fix linters issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
   validates :comment_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :like_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
