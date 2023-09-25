c452b44f9fbbc467fef06bef6153d3247b037125
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-09-25 21:43:14 +0530

    Write test for User and Post in The Controller

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  describe 'GET /users/:user_id/posts' do
+    it 'returns a successful response' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('Welcome to posts')
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'returns a successful response' do
+      user = User.create(name: 'Test User')
+      post = user.posts.create(title: 'Test Post', text: 'Test Content') # Ensure 'content' attribute is correct
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      user = User.create(name: 'Test User')
+      post = user.posts.create(title: 'Test Post', text: 'Test Content') # Ensure 'content' attribute is correct
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      user = User.create(name: 'Test User')
+      post = user.posts.create(title: 'Test Post', text: 'Test Content') # Ensure 'content' attribute is correct
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include('This is shown what posts haven')
+    end
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns a successful response' do
+      User.create(name: 'Test User')
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      User.create(name: 'Test User')
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body for index' do
+      User.create(name: 'Test User')
+      get users_path
+      expect(response.body).to include('Welcome to Bloggy')
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'returns a successful response' do
+      user = User.create(name: 'Test User')
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      user = User.create(name: 'Test User')
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body for show' do
+      user = User.create(name: 'Test User')
+      get user_path(user)
+      expect(response.body).to include('Hello this is what to show')
+    end
+  end
+end
