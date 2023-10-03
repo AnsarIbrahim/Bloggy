1ce358dd2a8880cab8fa680834e3a434810d8031
MARVIN <o.silverygold@gmail.com>
2023-10-03 14:12:39 +0300

    Update after sign in path for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :set_current_user
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || users_path
+  end
+  
+  private
+    def set_current_user
+      @current_user = current_user
+    end
+  end
