5d520644fc03753fffe0a5dd6d250f1f55cc976e
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-04 14:40:25 +0530

    Install CanCanCan and set post to delete function and create delete button

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  
+
   devise_for :users, controllers: {
                        registrations: "users/registrations",
                        sessions: "users/sessions",
                      }
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :new]
       resources :likes, only: [:create, :new]
     end
